e875472bb42463bfe56127f9bf923c088044a812
Evan Dorn <evan@lrdesign.com>
2009-12-23 18:07:26 -0800

    implemented basic login/logout mechanics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,40 +1,45 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper      
-  
 
- # Stores a headline for later rendering by the layout
- def set_headline(headline)
-   content_for(:headline, headline)
- end
- 
- 
- # Passes the supplied block to the named partial
- def block_to_partial(partial_name, options = {}, &block)    
-   # replace :id with :cssid and :class with :cssclass
-   if options[:id]
-     options[:cssid] = options.delete(:id)
-   else
-     options[:cssid] = "" if options[:cssid].nil?
-   end
-   if options[:class]
-     options[:cssclass] = options.delete(:class) 
-   else
-     options[:cssclass] = "" if options[:cssclass].nil?    
-   end   
-     
-   options.merge!(:body => capture(&block))
-   concat(render(:partial => partial_name, :locals => options))
- end
- 
- 
- # pass { :nolabel => true } to replace the label with a spacer   
- # pass { :required => true } to dispay as a required field  
- # pass { :text => "foo" } to override the label text
- def labeled_input(form, field, options = {}) 
-   options[:text] = "&nbsp;" if options[:nolabel]
-   options.reverse_merge!(:text => nil, :size => nil, :required => false)
-   options.merge!(:form => form, :field => field)
-   render(:partial => 'shared/labeled_input', :locals => options)  
- end  
-  
+
+  # Stores a headline for later rendering by the layout
+  def set_headline(headline)
+    content_for(:headline, headline)
+  end
+
+
+  # Passes the supplied block to the named partial
+  def block_to_partial(partial_name, options = {}, &block)    
+    # replace :id with :cssid and :class with :cssclass
+    if options[:id]
+      options[:cssid] = options.delete(:id)
+    else
+      options[:cssid] = "" if options[:cssid].nil?
+    end
+    if options[:class]
+      options[:cssclass] = options.delete(:class) 
+    else
+      options[:cssclass] = "" if options[:cssclass].nil?    
+    end   
+
+    options.merge!(:body => capture(&block))
+    concat(render(:partial => partial_name, :locals => options))
+  end
+
+
+  # pass { :nolabel => true } to replace the label with a spacer   
+  # pass { :required => true } to dispay as a required field  
+  # pass { :text => "foo" } to override the label text
+  def labeled_input(form, field, options = {}) 
+    options[:text] = "&nbsp;" if options[:nolabel]
+    options.reverse_merge!(:text => nil, :size => nil, :required => false)
+    options.merge!(:form => form, :field => field)
+    render(:partial => 'shared/labeled_input', :locals => options)  
+  end  
+
+  # same function as application_controller.rb's logged_in? method
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users   
-  map.resource       :user_session        
+  map.resources :user_sessions        
 
+  map.logout '/logout', :controller => "user_sessions", :action => "destroy"
+  map.login '/login', :controller => "user_sessions", :action => "new"
+  
   map.home '/', :controller => 'home', :action => 'index'             
 end
