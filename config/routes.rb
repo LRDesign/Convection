8686b8b1ef2129f2a695cb9f03d6ba80e9288793
Evan Dorn <evan@lrdesign.com>
2009-12-23 17:43:29 -0800

    added user sessions, removed logs from repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
-                                
- map.home '/', :controller => 'home', :action => 'index'             
+  map.resources :users   
+  map.resource       :user_session        
+
+  map.home '/', :controller => 'home', :action => 'index'             
 end
