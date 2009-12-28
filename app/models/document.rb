0404272e8a1aa5cf2422507763a4b69649a415fe
Judson <nyarly@gmail.com>
2009-12-28 14:24:48 -0800

    Files upload to RAILS_ROOT/file-storage/datas.  Spec expanded to check uploads

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ActiveRecord::Base       
-  has_attached_file :data                 
+  has_attached_file :data, :path => ":rails_root/file-storage/:attachment/:id/:style/:filename"
   belongs_to :user                     
   validates_attachment_presence :data  
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,16 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'digest'
+require 'fileutils'
 
 describe Document do
   before(:each) do
+    @file = File.new(File.join(File.dirname(__FILE__),
+                               "..",
+                               "fixtures",
+                               "5k.png"), 'rb')
+
     @valid_attributes = {
-      :name => "value for name",
+      :name => "a_file",
       :description => "value for description",
-      :data_update_at => Time.now,
+      :data => @file,
       :user_id => "1"
     }
   end
 
+  after do
+    @file.close
+  end
+
   it "should create a new instance given valid attributes" do
-    # Document.create!(@valid_attributes)
+    doc = Document.create!(@valid_attributes)
+
+    source_sum = Digest::SHA512::file(@file.path)
+    dest_sum = nil
+    proc do
+      dest_sum = Digest::SHA512::file("#{RAILS_ROOT}/file-storage/datas/#{doc.id}/original/5k.png")
+    end.should_not raise_error
+
+    dest_sum.should == source_sum
+  end
+
+  after(:all) do
+    FileUtils::rm_rf("#{RAILS_ROOT}/file-storage/datas")
+    
   end
 end
