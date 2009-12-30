fc934f8db8f3a0c938cb6d0e3c381fdf3331e3b3
Evan Dorn <evan@lrdesign.com>
2009-12-29 16:32:16 -0800

    fixed a view spec

--- spec/views/admin/users/new.html.haml_spec.rb
+++ spec/views/admin/users/new.html.haml_spec.rb
@@ -16,7 +16,7 @@ describe "/users/new.html.haml" do
   
   it "should render new form" do
       
-    response.should have_tag("form[action=?][method=post]", users_path) do
+    response.should have_tag("form[action=?][method=post]", admin_users_path) do
       with_tag("input#user_login[name=?]", "user[login]")
       with_tag("input#user_email[name=?]", "user[email]")
       with_tag("input#user_name[name=?]", "user[name]")
