d0c2afe62d90899136eb941c9b33a3ccd22aa89c
Evan Dorn <evan@lrdesign.com>
2010-01-02 00:00:29 -0800

    added loading of gems from vendor

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,11 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::Initializer.run do |config|
+Rails::Initializer.run do |config| 
+  
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir|    
+    File.directory?(lib = "#{dir}/lib") ? lib : dir
+  end  
   
   # Required gems 
   config.gem "haml"
