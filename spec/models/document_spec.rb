d737a75e9752efc1d021537410394abddc698fd0
Judson <nyarly@gmail.com>
2010-01-17 15:35:07 -0800

    Fixed document_spec to delete only files related to spec run

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -31,26 +31,24 @@ describe Document do
       :data => @file,
       :user_id => "1"
     }
+    @doc = nil
   end
+
 
   after do
     @file.close
+    FileUtils::rm_rf("#{RAILS_ROOT}/file-storage/datas/#{@doc.id}") unless @doc.nil?
   end
 
   it "should create a new instance given valid attributes" do
-    doc = Document.create!(@valid_attributes)
+    @doc = Document.create!(@valid_attributes)
 
     source_sum = Digest::SHA512::file(@file.path)
     dest_sum = nil
     proc do
-      dest_sum = Digest::SHA512::file("#{RAILS_ROOT}/file-storage/datas/#{doc.id}/original/5k.png")
+      dest_sum = Digest::SHA512::file("#{RAILS_ROOT}/file-storage/datas/#{@doc.id}/original/5k.png")
     end.should_not raise_error
 
     dest_sum.should == source_sum
-  end
-
-  after(:all) do
-    FileUtils::rm_rf("#{RAILS_ROOT}/file-storage/datas")
-    
   end
 end
