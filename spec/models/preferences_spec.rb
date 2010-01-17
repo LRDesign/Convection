e7e69481c1349dc6eb20d623c66f9b524b98cbed
Evan Dorn <evan@lrdesign.com>
2010-01-17 14:36:38 -0800

    annotated models

--- spec/models/preferences_spec.rb
+++ spec/models/preferences_spec.rb
@@ -2,19 +2,22 @@
 #
 # Table name: preferences
 #
-#  id                  :integer(4)      not null, primary key
-#  domain              :string(255)
-#  site_name           :string(255)
-#  smtp_server         :string(255)
-#  smtp_port           :integer(4)
-#  smtp_uses_ssl       :boolean(1)
-#  smtp_username       :string(255)
-#  smtp_password       :string(255)
-#  email_notifications :boolean(1)
-#  analytics           :text
-#  logo_url            :string(255)
-#  created_at          :datetime
-#  updated_at          :datetime
+#  id                    :integer(4)      not null, primary key
+#  domain                :string(255)
+#  site_name             :string(255)
+#  smtp_server           :string(255)
+#  smtp_port             :integer(4)
+#  smtp_uses_tls         :boolean(1)
+#  smtp_username         :string(255)
+#  smtp_password         :string(255)
+#  email_notifications   :boolean(1)      default(TRUE)
+#  allow_password_resets :boolean(1)      default(TRUE)
+#  require_ssl           :boolean(1)
+#  maximum_file_size     :integer(4)
+#  analytics             :text
+#  logo_url              :string(255)
+#  created_at            :datetime
+#  updated_at            :datetime
 #
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')    
