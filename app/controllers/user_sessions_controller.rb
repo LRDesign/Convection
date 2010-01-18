b0ae4dc2c1390bea133518e453ab8be0b92a1f8b
Evan Dorn <evan@lrdesign.com>
2010-01-17 20:09:14 -0800

    styled flash messages

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
+      flash[:success] = "Login successful!"
       redirect_back_or_default home_url
     else
       render :action => :new
@@ -18,7 +18,7 @@ class UserSessionsController < ApplicationController
  
   def destroy
     current_user_session.destroy
-    flash[:notice] = "Logout successful!"
+    flash[:success] = "You have logged out."
     redirect_back_or_default new_user_session_url
   end
 end
