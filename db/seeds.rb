f13800bd810a0b5ec2853008b25d455d6d6fe22e
Judson <nyarly@gmail.com>
2010-01-18 16:58:56 -0800

    Applied group_authz rake task to Convection

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,21 @@ Preferences.create!(
   :require_ssl => false           # Site doesn't require SSL connections
 )
 
+admin = Group.create!(
+  :name => "Administration"
+)
+
+module GroupAuthz
+  module PermissionSeeds
+    def self.create_permission(user, controller, action = nil, subject_id = nil)
+      GroupAuthz::Permission.create!(
+        :group_id => user.id,
+        :controller => controller,
+        :action => action,
+        :subject_id => subject_id
+      )
+    end
+
+    #create_permission(admin, "admin/permissions")
+  end
+end
