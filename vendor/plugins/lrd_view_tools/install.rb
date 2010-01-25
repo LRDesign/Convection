d9107f62fa8726a21baf7134f13a5d4053e431d5
Evan Dorn <evan@lrdesign.com>
2010-01-25 12:47:34 -0800

    added default stylesheets to view tools installer

--- vendor/plugins/lrd_view_tools/install.rb
+++ vendor/plugins/lrd_view_tools/install.rb
@@ -2,10 +2,30 @@
 
 require 'ftools'
 
-plugins_dir = File.expand_path(".")
-lrd_view_tools_dir = File.join(plugins_dir, 'lrd_view_tools')
-root_dir = File.join(lrd_view_tools_dir, '..', '..', '..')
 
-File.copy File.join(lrd_view_tools_dir, 'stylesheets', 'debug.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'debug.sass')
-File.copy File.join(lrd_view_tools_dir, 'images', 'blank.gif'), File.join(root_dir, 'public', 'images', 'blank.gif')
-File.copy File.join(lrd_view_tools_dir, 'images', 'check.png'), File.join(root_dir, 'public', 'images', 'check.png')
+
+File.mkdir_p File.join(root_dir, 'public', 'stylesheets', 'sass') 
+
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'reset.css'), File.join(root_dir, 'public', 'stylesheets', 'reset.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'debug.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'debug.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'constants.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'constants.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'screen.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'screen.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'universal.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'universal.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'utility.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'utility.sass')
+install_file File.join(lrd_view_tools_dir, 'stylesheets', 'sass', 'print.sass'), File.join(root_dir, 'public', 'stylesheets', 'sass', 'print.sass')
+
+install_file File.join(lrd_view_tools_dir, 'images', 'blank.gif'), File.join(root_dir, 'public', 'images', 'blank.gif')
+install_file File.join(lrd_view_tools_dir, 'images', 'check.png'), File.join(root_dir, 'public', 'images', 'check.png')
+
+
+def install_file(source,dest)   
+  plugins_dir = File.expand_path(".")
+  lrd_view_tools_dir = File.join(plugins_dir, 'lrd_view_tools')
+  root_dir = File.join(lrd_view_tools_dir, '..', '..', '..')
+  
+  if File.exists?(dest)
+    p "Not installing #{dest}, file exists"   
+  else 
+    File.copy(source, dest)
+  end    
+end
