3b38e8094e00f1f6ebe67258a50b142007523bb2
Evan Dorn <evan@lrdesign.com>
2010-01-26 14:51:56 -0800

    converted README to rdoc

--- vendor/plugins/lrd_view_tools/init.rb
+++
@@ -1,4 +0,0 @@
-require File.join(File.dirname(__FILE__), "lib", "lrd_view_helper")  
-require File.join(File.dirname(__FILE__), "lib", "lrd_debug_helper")  
-ActionView::Base.send :include, LRD::ViewHelper  
-ActionView::Base.send :include, LRD::DebugHelper  
--- vendor/plugins/lrd_view_tools/spec/lrd_view_tools_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/spec_helper'
-
-# include LRDViewHelpers
-
-describe "LRDViewHelpers" do
-  it "should have a pending spec"
-  
-  
-end
