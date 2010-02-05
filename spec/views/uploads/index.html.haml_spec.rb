bf13c9854508f5c971c032318a17c0ca1cfc26cc
Evan Dorn <evan@lrdesign.com>
2010-02-04 22:48:27 -0800

    fixed view uploads index spec

--- spec/views/uploads/index.html.haml_spec.rb
+++ spec/views/uploads/index.html.haml_spec.rb
@@ -3,8 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/uploads/index" do
   before(:each) do
     assigns[:documents] = [
-      Factory.build(:document),
-      Factory.build(:document)
+      Factory.create(:document),
+      Factory.create(:document)
       ]
     render 'uploads/index'
   end
