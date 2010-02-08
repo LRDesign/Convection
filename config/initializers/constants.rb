9f29e221b95e3de16019ec721f1162f39bc1feb2
Evan Dorn <evan@lrdesign.com>
2010-02-08 12:44:02 -0800

    cleaned up views, fixed flash

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,14 @@
-CONVECTION_VERSION = "v0.0.0"
+CONVECTION_VERSION = "v0.9.0"                                                      
+
+my_formats = {
+	:short_date => '%Y-%m-%d',
+	:date => '%Y-%m-%d',
+	:short_time => "%I:%M%p ",
+	:time => "%I:%M%p ",
+  :month_year => "%b %Y",
+	:long_string_date => "%B %e, %Y",
+  :month_day_year_words => "%B %e, %Y",
+  :date_and_time => "%B %d, %Y %l:%M %p"		  
+}
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(my_formats)
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(my_formats)
