25c43c7ea2e41125ef647ea2ec58d1d444521ab1
Judson <nyarly@gmail.com>
2010-02-09 00:34:30 -0800

    Refactor of GroupAuthz to decouple authorization from controllers/users/or groups.

--- vendor/plugins/group_authz/app/helpers/group_authz_helper.rb
+++ vendor/plugins/group_authz/app/helpers/group_authz_helper.rb
@@ -7,27 +7,14 @@ module GroupAuthz
   end
 
   module Helper
-    def authorized?(criteria={})
-      current_user = AuthnFacade.current_user(self)
-      return false if current_user.blank?
-      controller_class_path = criteria[:controller]
-      controller_class_path = controller_class_path.to_s if Symbol === controller_class_path
-      controller_class = ::ApplicationController
-
-      if String === controller_class_path
-        controller_class_name = controller_class_path.camelize + "Controller"
-        begin 
-          controller_class = controller_class_name.constantize
-        rescue NameError
-          controller_class = ::ApplicationController
-        end
-      else
-        controller_class = controller.class
+    def authorized?(criteria=nil)
+      criteria ||= {}
+      criteria = {:action => action_name, :id => params[:id]}.merge(criteria)
+      unless criteria.has_key?(:group) or criteria.has_key?(:user)
+        criteria[:user] = AuthnFacade.current_user(self)
       end
 
-      criteria = {:action => action_name, :id => params[:id]}.merge(criteria)
-
-      controller_class.can_authorize(current_user, criteria)
+      GroupAuthz.is_authorized?(criteria)
     end 
         
     # returns an array of group names and ids (suitable for select_tag)
--- vendor/plugins/group_authz/lib/group_authz.rb
+++ vendor/plugins/group_authz/lib/group_authz.rb
@@ -1,6 +1,62 @@
 require 'group_authz_helper'
 
 module GroupAuthz
+  def self.is_authorized?(criteria={})
+    criteria ||= {}
+
+    controller_class = ::ApplicationController
+
+    case criteria[:controller]
+    when Class
+      controller_class = criteria[:controller]
+    when String, Symbol
+      controller_class_name = criteria[:controller].to_s.camelize + "Controller"
+      begin 
+        controller_class = controller_class_name.constantize
+      rescue NameError
+      end
+    end
+
+    #TODO Fail if controller unspecified?
+
+    criteria[:group] = criteria.has_key?(:group) ? [*criteria[:group]] : []
+    if criteria.has_key?(:user)
+      criteria[:group] += criteria[:user].groups
+    end
+    criteria[:groups] = criteria[:group]
+
+    #TODO Fail if group unspecified and user unspecified?
+
+    criteria[:action_aliases] = [*criteria[:action]].map do |action|
+      controller_class.grant_aliases_for(action)
+    end.flatten
+
+    controller_class.authorization_blocks.each do |prok|
+      approval = prok.call(criteria[:user], criteria) #Tempted to remove the user param
+      next if approval == false
+      next if approval.blank?
+      return true
+    end
+
+    select_on = {
+      :group_id => criteria[:group].map{|grp| grp.id},
+      :controller => controller_class.controller_path,
+      :action => nil,
+      :subject_id => nil
+    }
+
+    permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
+    return true unless permissions.nil?
+
+    select_on[:action] = [*criteria[:action]] + criteria[:action_aliases]
+    permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
+    return true unless permissions.nil?
+
+    select_on[:subject_id] = criteria[:id]
+    permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
+    return (not permissions.nil?)
+  end
+
   module Application
     def self.included(klass)
       klass.extend(ClassMethods)
@@ -18,12 +74,16 @@ module GroupAuthz
 
     def check_authorized
       current_user = AuthnFacade.current_user(self)
-
-      criteria = {:action => action_name, :id => params[:id]}
-
       return false if current_user.blank?
 
-      if self.class.can_authorize(current_user, criteria)
+      criteria = {
+        :user => current_user, 
+        :controller => self.class,
+        :action => action_name, 
+        :id => params[:id]
+      }
+
+      if GroupAuthz.is_authorized?(criteria)
         flash[:group_authorization] = true
         return true
       else
@@ -34,44 +94,6 @@ module GroupAuthz
     end
 
     module ClassMethods
-      def can_authorize(user, criteria=nil)
-        criteria ||= {}
-
-        groups = criteria[:group] ? [criteria[:group]] : user.groups
-
-        (read_inheritable_attribute(:dynamic_authorization_procs) || []).each do |prok|
-          approval = prok.call(user, criteria)
-          next if approval == false
-          next if approval.blank?
-          return true
-        end
-
-        select_on = {
-          :group_id => groups.map{|grp| grp.id},
-          :controller => controller_path,
-          :action => nil,
-          :subject_id => nil
-        }
-
-        permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
-        return true unless permissions.nil?
-
-        action_grants = [criteria[:action]]
-        grant_aliases = read_inheritable_attribute(:grant_alias_hash)
-
-        if not grant_aliases.nil? and grant_aliases.has_key?(criteria[:action])
-          action_grants += grant_aliases[criteria[:action]]
-        end
-
-        select_on[:action] = action_grants
-        permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
-        return true unless permissions.nil?
-
-        select_on[:subject_id] = criteria[:id]
-        permissions = GroupAuthz::Permission.find(:first, :conditions => select_on)
-        return (not permissions.nil?)
-      end
-
       def needs_authorization(*actions)
         before_filter CheckAuthorization
         if actions.empty?
@@ -90,9 +112,23 @@ module GroupAuthz
         end
         write_inheritable_attribute(:grant_alias_hash, aliases)
       end
+      
+      def grant_aliases_for(action)
+        grant_aliases = read_inheritable_attribute(:grant_alias_hash)
+
+        if not grant_aliases.nil? and grant_aliases.has_key?(action)
+          return grant_aliases[action]
+        else
+          return []
+        end
+      end
 
       def dynamic_authorization(&block)
         write_inheritable_array(:dynamic_authorization_procs, [proc &block])
+      end
+
+      def authorization_procs
+        read_inheritable_attribute(:dynamic_authorization_procs) || []
       end
 
       def admin_authorized(*actions)
