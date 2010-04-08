d9b90717e1df938b1ac91b2b7cde6982f5418aa3
Evan Dorn <evan@lrdesign.com>
2010-04-08 00:26:19 -0700

    added migration for google analytics and reannotated preferences

--- spec/views/documents/show.html.haml_spec.rb
+++ spec/views/documents/show.html.haml_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 describe "/documents/show.html.haml" do
   include DocumentsHelper
   
-  before(:each) do       
+  before(:each) do            
+    activate_authlogic
     @document = Factory(:document)
     @document.user = Factory(:user)
     assigns[:document] = @document
@@ -13,6 +14,10 @@ describe "/documents/show.html.haml" do
     render "/documents/show.html.haml"
     response.should have_text(/#{@document.name}/)
     response.should have_text(/#{@document.description}/)
+  end        
+  
+  it "should include google analytics code if the preferences are set" do
+    
   end
 end
 
