50e95e4addb682ca0e63326130746f49691b639d
Judson <nyarly@gmail.com>
2010-04-08 17:33:18 -0700

    LogicalAuthz becoming generated

---
+++ vendor/plugins/LogicalAuthz/generators/logical_authz_models/logical_authz_models_generator.rb
@@ -0,0 +1,11 @@
+module LogicalAuthz
+  class ModelsGenerator < Rails::Generators::Base
+    record do |manifest|
+      manifest.class_collisions "Group", "Permission"
+      manifest.file "app/models/group.rb", "app/models/group.rb", :collision => :skip
+      manifest.file "app/models/permission.rb", "app/models/permission.rb", :collision => :skip
+      manifest.migration_file "setup_logical_authz.rb", "db/migrate"
+    end
+  end
+end
+
--- vendor/plugins/LogicalAuthz/generators/logical_authz_models/templates/setup_logical_authz.rb
+++ vendor/plugins/LogicalAuthz/generators/logical_authz_models/templates/setup_logical_authz.rb
@@ -17,3 +17,19 @@ class CreateGroups < ActiveRecord::Migration
     drop_table :groups_users
   end
 end
+
+class CreatePermissions < ActiveRecord::Migration
+  def self.up
+    create_table :permissions do |t|
+      t.references :group
+      t.string :controller
+      t.string :action
+      t.integer :subject_id
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :permissions
+  end
+end
