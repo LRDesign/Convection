ff93f0be2d5436c0bc768c48f64fa106ca6c43e3
Judson <nyarly@gmail.com>
2010-04-08 18:03:51 -0700

    Functioning generator

--- vendor/plugins/LogicalAuthz/generators/logical_authz_models/logical_authz_models_generator.rb
+++ vendor/plugins/LogicalAuthz/generators/logical_authz_models/logical_authz_models_generator.rb
@@ -4,7 +4,7 @@ class LogicalAuthzModelsGenerator < Rails::Generator::Base
       manifest.class_collisions "Group", "Permission"
       manifest.file "app/models/group.rb", "app/models/group.rb", :collision => :skip
       manifest.file "app/models/permission.rb", "app/models/permission.rb", :collision => :skip
-      manifest.migration_template "setup_logical_authz.rb", "db/migrate"
+      manifest.migration_template "setup_logical_authz.rb", "db/migrate", :migration_file_name => "setup_logical_authz"
     end
   end
 end
