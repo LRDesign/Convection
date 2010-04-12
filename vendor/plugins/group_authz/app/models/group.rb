eb3bbbbb82779cae7d8f4780b7fba17b0f9a4ba2
Austin Fonacier <austinrf@gmail.com>
2010-04-11 17:40:35 -0700

    Adding group's visible documents

--- vendor/plugins/group_authz/app/models/group.rb
+++ vendor/plugins/group_authz/app/models/group.rb
@@ -21,6 +21,14 @@ class Group < ActiveRecord::Base
     !self.permissions.find(:first, :conditions => conditions.merge!(:subject_id => object.id)).nil?
   end
   
+  # Returns the visible documents that the group has access to
+  def visible_documents
+    permissions = self.permissions.find(:all, 
+                    :conditions => ["controller = ? AND action = ?", "documents", "show"])
+    ids = permissions.collect{|p| p.subject_id}
+    Document.find(:all, :conditions => {:id => ids||[]})
+  end
+  
   class << self
     def admin_group
       self.find_by_name('Administration')
