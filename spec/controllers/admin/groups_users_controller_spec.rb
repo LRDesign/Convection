dea6eeb217a5cc6846d55e7071f8389f8ecde941
Evan Dorn <evan@lrdesign.com>
2010-04-15 10:07:44 -0700

    attempted addition of specs to reproduce Authz problem

---
+++ spec/controllers/admin/groups_users_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+                                  
+describe Admin::GroupsUsersController do      
+  
+  # One of the things I've tried in attempts to get these specs to run
+  # controller_name 'admin/groups_users'
+
+  before(:each) do          
+    activate_authlogic
+    @admin = login_as( Factory.create(:admin) )
+    request.env['HTTP_REFERER'] = "http://test.host/previous/page"
+    @user = Factory.create(:user)
+    @user.groups.clear
+    @group = Factory.create(:group, :name => "registered")   
+
+    # One of the things I've tried in attempts to get these specs to run
+    # @controller = 'admin/groups_users'
+  end
+  
+  it "group should exist" do
+    @group.should_not be_nil
+  end
+
+  describe "POST 'create'" do
+    it "should succeed" do
+      post 'create', :user_id => @user.id, :group_id => @group.id
+      response.should be_redirect
+    end
+    
+    it "should create the association" do
+      post 'create', :user_id => @user.id, :group_id => @group.id
+      @user.reload.groups.should include(@group)        
+    end    
+  end
+
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @user.groups << @group
+    end
+    
+    it "should be successful" do
+      delete 'destroy', :user_id => @user.id, :group_id => @group.id      
+      response.should be_redirect
+    end
+
+    it "should delete the association" do
+      delete 'destroy', :user_id => @user.id, :group_id => @group.id
+      @user.reload.groups.should_not include(@group)        
+    end    
+    
+    it "should not allow removing a user from all_users"     
+  end
+end
