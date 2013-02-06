6f8a4112025551883f415f31946ceed52d3c86b8
Evan Dorn <evan@lrdesign.com>
2013-02-06 11:23:56 -0800

    Disable xml parsing: CVE-2013-0156

--- config/environment.rb
+++ config/environment.rb
@@ -6,16 +6,16 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::Initializer.run do |config| 
-  
-  # Required gems 
+Rails::Initializer.run do |config|
+
+  # Required gems
   config.gem "haml"
-  config.gem "authlogic", :version => ">= 2.0.9"
-  config.gem "chronic"
+  config.gem "authlogic", :version => "~> 2.1.0"
+  config.gem "chronic", :version => "~> 0.2.3"
   config.gem "tlsmail", :version => ">= 0.0.1"
   config.gem "will_paginate", :version => ">= 2.3.12"
   config.gem 'hoptoad_notifier'
- 
+
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.active_record.default_timezone = :utc
